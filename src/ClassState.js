3231e88a395dccd41146a05bb21f3bb9d91e6b49
Luis Herrera <luis1818.lh@gmail.com>
2024-01-19 16:28:11 -0500

    Se crean los archivos ClassState y UseState, class component y function component

---
+++ src/ClassState.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+class ClassState extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            error: true
+        }
+    }
+    render() {
+
+        return (
+            <div>
+                <h2>Eliminar {this.props.name}</h2>
+                <p> Por favor escribe el codigo de seguridad</p>
+                {this.state.error &&(
+                <p>Error: el Código es incorrecto</p>
+                )}
+                <input placeholder="Código de seguridad" />
+                <button
+                    onClick={()=> this.setState({error: !this.state.error})}
+                >Comprobar</button>
+            </div>
+        )
+    }
+}
+export { ClassState }

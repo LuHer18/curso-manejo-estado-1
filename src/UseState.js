29df9dff8ab7f3a7e0ff6869c318ea5d19d836aa
Luis Herrera <luis1818.lh@gmail.com>
2024-01-19 16:50:37 -0500

    Se crea un UseEffect en el archivo UseState

--- src/UseState.js
+++ src/UseState.js
@@ -1,6 +1,20 @@
 import React from "react"
 function UseState({name}){
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log('Empezando el efecto')
+        if(loading){
+            setTimeout(()=>{
+                console.log('Haciendo la validación')
+                setLoading(false)
+                console.log('Terminando la validación')
+            }, 3000)
+        }
+        console.log('Terminando el efecto el efecto')
+    }, [loading])
+   
     return (
         <div>
             <h2>Eliminar {name}</h2>
@@ -8,10 +22,13 @@ function UseState({name}){
             {error &&(
                 <p>Error: el Código es incorrecto</p>
             )}
+            {loading &&(
+                <p>Cargando...</p>
+            )}
 
             <input placeholder="Código de seguridad"/>
             <button
-                onClick={()=> setError(!error)}
+                onClick={()=> setLoading(true)}
             >Comprobar</button>
         </div>
     )

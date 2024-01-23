960b469bf5fd323b24bd45af122f9b30a3332b11
Luis Herrera <luis1818.lh@gmail.com>
2024-01-23 12:38:27 -0500

    Se usa un estado global como objeto que almacena los demás state

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,24 +1,64 @@
 import React from "react"
+import { Loading } from "./Loading";
+
+const SECURITY_CODE = 'paradigma'
 
 class ClassState extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            error: true
+            value: '',
+            error: false,
+            loading: false,
         }
     }
+/*     UNSAFE_componentWillMount(){
+        console.log("componentWillMount")
+    }
+     */
+  
+/*     componentDidMount(){
+        console.log("componentDidMount")
+    } */
+
+    componentDidUpdate(){
+        console.log('actualización')
+        if(!!this.state.loading){
+            setTimeout(()=>{
+                console.log('Haciendo la validación')
+                if(SECURITY_CODE === this.state.value){
+                    this.setState({loading: false})
+                } else {
+                    this.setState({error: true, loading: false, })
+                }
+                console.log('Terminando la validación')
+            }, 3000)
+        }
+        
+    }
+    
+
     render() {
 
         return (
             <div>
                 <h2>Eliminar {this.props.name}</h2>
                 <p> Por favor escribe el codigo de seguridad</p>
-                {this.state.error &&(
+                {(this.state.error && !this.state.loading) &&(
                 <p>Error: el Código es incorrecto</p>
+                 )}
+                {this.state.loading &&(
+                <Loading/>
                 )}
-                <input placeholder="Código de seguridad" />
+                <input
+                    placeholder="Código de seguridad"
+                    value={this.state.value}
+                    onChange={(e)=>{
+                        this.setState({value: e.target.value})
+                    }}
+                />
                 <button
-                    onClick={()=> this.setState({error: !this.state.error})}
+                    onClick={()=> this.setState({loading: true, error: false})}
                 >Comprobar</button>
             </div>
         )
--- src/UseState.js
+++ src/UseState.js
@@ -1,37 +1,144 @@
 import React from "react"
-function UseState({name}){
+
+const SECURITY_CODE = 'paradigma'
+
+function UseState({ name }) {
+    const [state, setState] = React.useState({
+        value: '',
+        error: false,
+        loading: false,
+        deleted: false,
+        confirmed: false,
+    })
+    const [value, setValue] = React.useState('');
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
+    const onConfirm = () => {
+        setState({
+            ...state,
+            loading: false,
+            error: false,
+            confirmed: true
+        })
+    }
+    const onError = () => {
+        setState({
+            ...state,
+            loading: false, 
+            error: true
+        })
+    }
+
+    const onWrite = (e) => {
+        setState({
+            ...state,
+            value: e.target.value
+        })
+    }
+
+    const onCheck = () => {
+        setState({
+            ...state,
+            loading: true
+        })
+    }
+    const onDelete = () => {
+        setState({
+            deleted: true,
+        })
+    }
+
+    const onCancel = () => {
+        setState({
+            ...state,
+            confirmed: false,
+            value: '',
+        })
+    }
+
+    const onReturn = () => {
+        setState({
+            ...state,
+            confirmed: false,
+            value: '',
+            deleted: false
+        })
+    }
+
     React.useEffect(() => {
         console.log('Empezando el efecto')
-        if(loading){
-            setTimeout(()=>{
+        if (state.loading) {
+            setTimeout(() => {
                 console.log('Haciendo la validación')
-                setLoading(false)
+
+                if (state.value === SECURITY_CODE) {
+                    onConfirm();
+                } else {
+                    onError();
+                }
+
                 console.log('Terminando la validación')
             }, 3000)
+            setState({
+                ...state,
+                error: false
+            }) // Posible BOOM
         }
         console.log('Terminando el efecto el efecto')
-    }, [loading])
-   
-    return (
-        <div>
-            <h2>Eliminar {name}</h2>
-            <p> Por favor escribe el codigo de seguridad</p>
-            {error &&(
-                <p>Error: el Código es incorrecto</p>
-            )}
-            {loading &&(
-                <p>Cargando...</p>
-            )}
+    }, [state.loading])
+    console.log(state.value)
 
-            <input placeholder="Código de seguridad"/>
-            <button
-                onClick={()=> setLoading(true)}
-            >Comprobar</button>
-        </div>
-    )
+
+    if (!state.deleted && !state.confirmed) {
+        return (
+            <div>
+                <h2>Eliminar {name}</h2>
+                <p> Por favor, escribe el codigo de seguridad para confirmar que quieres eliminar</p>
+                {state.error && (
+                    <p>Error: el Código es incorrecto</p>
+                )}
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+
+                <input
+                    placeholder="Código de seguridad"
+                    value={state.value}
+                    onChange={(e) => {
+                        onWrite(e)
+                    }} />
+                <button
+                    onClick={() =>onCheck() }
+                >Comprobar</button>
+            </div>
+        )
+    }else if ( state.confirmed && !state.deleted){
+        return(
+            <>
+            <h1>Estado de confirmación</h1>
+            <p>¿Seguro que quieres eliminar UseState?</p>
+            <button onClick={()=>{
+                    onDelete()
+                }} 
+            > Si, eliminar</button>
+            <button 
+                onClick={()=>{
+                    onCancel();
+                }}   
+            > No, volver</button>
+            </>
+        );
+    } else {
+        return(
+            <>
+            <h2>Eliminado {name} con éxito</h2>
+            <button onClick={()=> {
+                onReturn()
+            }}>Recuperar {name}</button>
+            </>
+        );
+    }
 }
 
-export {UseState}
+export { UseState }
